be14a5c8413853190e215fc174b23d83b80dae03
KevinEppacher <kevin-eppacher@hotmail.de>
2024-10-17 20:58:05 +0200

    Moveit Config

---
+++ Bewegungsanalyse/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/kevin/Documents/Master/3_Semester/Produktentwicklung/Awesome_Gripper/Bewegungsanalyse/catkin_ws/src
+# Build directory: /home/kevin/Documents/Master/3_Semester/Produktentwicklung/Awesome_Gripper/Bewegungsanalyse/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("six_dofra_config_v1")
+subdirs("urdf_6_dofra_v3")
